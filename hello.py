de4d7e8321aa204320073e92680b7899a2eea594
4b3g0088 <4b3g0088@debian>
2025-10-22 10:26:44 +0800

    add get sys info. to hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
-print("Hello world")
+import psutil
+import time
 
+print('hello world')
+
+time.sleep(1) 
+
+cpu_percent = psutil.cpu_percent(interval=None) 
+print(f'CPU Usage: {cpu_percent}%')
+
+mem = psutil.virtual_memory()
+gb_conversion = 1024**3 # Bytes 轉 GB 
+
+print(f'Total Memory: {mem.total / gb_conversion:.2f} GB')
+print(f'Used Memory: {mem.used / gb_conversion:.2f} GB')
+print(f'Memory Usage: {mem.percent}%')
+
+disk = psutil.disk_usage('/') 
+
+print(f'Total Disk Space: {disk.total / gb_conversion:.2f} GB')
+print(f'Used Disk Space: {disk.used / gb_conversion:.2f} GB')
+print(f'Disk Usage: {disk.percent}%')
